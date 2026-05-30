3829126080841a79caadc24e7bc0f64742ee5f5e
mostandmore4u <mostandmore4u@gmail.com>
2026-05-31 00:17:10 +0530

    :roboshop v2 common group

--- 08-shipping.sh
+++ 08-shipping.sh
@@ -5,18 +5,18 @@ MYSQL_HOST=mysql.subbudevops.online
 source ./common.sh
 check_root
 
-java_setup
 app_setup
+java_setup
 systemd_setup
 
-dnf install mysql -y
+dnf install mysql -y &>>$LOGS_FILE
 VALIDATE $? "Installing MySQL client"
 
 mysql -h $MYSQL_HOST -u root -pRoboShop@1 -e "use cities" &>>$LOGS_FILE
 if [ $? -ne 0 ]; then
-    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql
-    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql
-    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOGS_FILE
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$LOGS_FILE
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOGS_FILE
     VALIDATE $? "Data loaded"
 else
     echo -e "Data already loaded ... $Y SKIPPING $N"
